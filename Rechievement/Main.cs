4f27bb80d51eea4da2e2d1b260c4ab9eff35d08b
zntly <masterboi5633@gmail.com>
2025-04-21 17:37:34 -0400

    Stuff

---
+++ Rechievement/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SML;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Reflection;
+using Server.Shared.Extensions;
+using SalemModLoaderUI;
+
+namespace Rechievement
+{
+    // Token: 0x02000004 RID: 4
+    [Mod.SalemMod]
+    public class Main
+    {
+        // Token: 0x06000008 RID: 8 RVA: 0x00002454 File Offset: 0x0000065
+        public void Start()
+        {
+            Console.WriteLine("i'm rechieving it");
+            /*AssetBundle assetBundleFromResources = FromAssetBundle.GetAssetBundleFromResources("Rechievement.resources.assetbundles.rechievement", Assembly.GetExecutingAssembly());
+            assetBundleFromResources.LoadAllAssets<Texture2D>().ForEach(delegate (Texture2D s)
+            {
+                Main.Textures.Add(s.name, s);
+            });*/
+            Settings.SettingsCache.SetValue("Re-earn Achievements", ModSettings.GetBool("Re-earn Achievements", "synapsium.rechievement"));
+            Settings.SettingsCache.SetValue("Extra Achievements", ModSettings.GetBool("Extra Achievements", "synapsium.rechievement"));
+            Settings.SettingsCache.SetValue("BToS2 Extra Achievements", ModSettings.GetBool("BToS2 Extra Achievements", "synapsium.rechievement"));
+        }
+    }
+}
---
+++ Rechievement/Settings.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using SML;
+using UnityEngine;
+using Server.Shared.Extensions;
+
+namespace Rechievement
+{
+    // Token: 0x02000003 RID: 3
+    [DynamicSettings]
+    public class Settings
+    {
+        // Token: 0x17000001 RID: 1
+        // (get) Token: 0x06000002 RID: 2 RVA: 0x00002058 File Offset: 0x00000258
+        public ModSettings.CheckboxSetting EarnAchievementsAgain
+        {
+            get
+            {
+                return new ModSettings.CheckboxSetting
+                {
+                    Name = "Re-earn Achievments",
+                    Description = "Allows you to earn achievements you already own",
+                    DefaultValue = true,
+                    AvailableInGame = false,
+                    Available = true,
+                    OnChanged = delegate (bool v)
+                    {
+                        Settings.SettingsCache.SetValue("Re-earn Achievements", v);
+                    }
+                };
+            }
+        }
+
+        public ModSettings.CheckboxSetting ExtraAchievements
+        {
+            get
+            {
+                return new ModSettings.CheckboxSetting
+                {
+                    Name = "Extra Achievements",
+                    Description = "Adds at least 1 extra achievement for most roles, and some general ones",
+                    DefaultValue = true,
+                    AvailableInGame = false,
+                    Available = true,
+                    OnChanged = delegate (bool v)
+                    {
+                        Settings.SettingsCache.SetValue("Extra Achievements", v);
+                    }
+                };
+            }
+        }
+
+        public ModSettings.CheckboxSetting BTOS2Achievements
+        {
+            get
+            {
+                return new ModSettings.CheckboxSetting
+                {
+                    Name = "BToS2 Extra Achievements",
+                    Description = "Adds extra achievements that are obtainable in BToS2 only, alongside allowing you to get extra achievements (only ones added by Rechievement) in BToS2",
+                    DefaultValue = true,
+                    AvailableInGame = false,
+                    Available = ModStates.IsEnabled("curtis.tuba.better.tos2"),
+                    OnChanged = delegate (bool v)
+                    {
+                        Settings.SettingsCache.SetValue("BToS2 Extra Achievements", v);
+                    }
+                };
+            }
+        }
+
+        public static Dictionary<string, bool> SettingsCache = new Dictionary<string, bool>()
+        {
+            {
+                "Re-earn Achievements",
+                true
+            },
+            {
+                "Extra Achievements",
+                true
+            },
+            {
+                "BToS2 Extra Achievements",
+                true
+            }
+        };
+    }
+}
